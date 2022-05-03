5eac9d711cd4695b82b758f5cd905f92bf6b4f90
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-05-03 17:25:31 -0300

    Added buttons feedback options

--- src/components/Widgetform.tsx
+++ src/components/Widgetform.tsx
@@ -1,5 +1,30 @@
 import { CloseButton } from "./CloseButton";
 
+const Buttons = {
+    BUG: {
+        title: "Reportar um bug",
+        image: {
+            source: "https://img.icons8.com/color/48/000000/bug.png",
+            alt: "Reportar um bug"
+        }
+    },
+    FEEDBACK: {
+        title: "Enviar feedback",
+        image: {
+            source: "https://img.icons8.com/color/48/000000/feedback.png",
+            alt: "Enviar feedback"
+        }
+    },
+    OTHER: {
+        title: "Outro",
+        image: {
+            source: "https://img.icons8.com/color/48/000000/question-mark.png",
+            alt: "Outros"
+        }
+    }
+
+}
+
 export function Widgetform() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -10,7 +35,25 @@ export function Widgetform() {
             </header>
 
             <div className="flex py-8 gap-2 w-full">
+                {Object.entries(Buttons).map(([key, value]) => {
+                    return (
+                        <>
+                            {console.log(key)}
+                            {console.log(value, 'value')}
+                            <button
+                                key={key}
+                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-zinc-800 hover:border-violet-500 focus:border-violet-500 focus:outline-none"
+                                onClick={() => console.log('clicado')}
+                                type="button"
+                            >
+                                <img src={value.image.source} alt={value.image.alt} />
+                                <span>{value.title}</span>
+                            </button>
+                        </>
+                    )
+                })
 
+                }
             </div>
 
             <footer className="text-xs text-neutral-400">
