acffd548f90001ab349b62b8044a13274c97be1a
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-05-03 21:42:43 -0300

    created buttons screen shot and send form of feedback content

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,7 +6,7 @@ export type FeedbackType = keyof typeof feedbackTypes
 
 export const feedbackTypes = {
     BUG: {
-        title: "Reportar um bug",
+        title: "Problemas",
         image: {
             source: "https://img.icons8.com/color/48/000000/bug.png",
             alt: "Reportar um bug"
@@ -32,16 +32,19 @@ export const feedbackTypes = {
 export function Widgetform() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+    const handleRestartFeedback = () => setFeedbackType(null);
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
 
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
-                <FeedbackContentStep />
-            )
-            }
+                <FeedbackContentStep
+                    feedbackType={feedbackType}
+                    onRestartFeedback={handleRestartFeedback}
+                />
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Feito por <a className="underline underline-offset-2" href="https://github.com/wesleyferreira97"> Wesley Ferreira</a>
