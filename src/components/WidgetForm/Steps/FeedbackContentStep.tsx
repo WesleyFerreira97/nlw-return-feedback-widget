d3710e6585809c6c333537e86c7e9cc1508bdb0a
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-05-03 23:08:48 -0300

    changes in feedback feature

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -7,9 +7,10 @@ import { ScreenshotButton } from '../ScreenshotButton';
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
     onRestartFeedback: () => void;
+    onFeedbackSent: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType, onRestartFeedback }: FeedbackContentStepProps) {
+export function FeedbackContentStep({ feedbackType, onRestartFeedback, onFeedbackSent }: FeedbackContentStepProps) {
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     const [screenshot, setScreenshot] = useState<string | null>(null);
     const [comment, setComment] = useState('');
@@ -17,6 +18,8 @@ export function FeedbackContentStep({ feedbackType, onRestartFeedback }: Feedbac
     function handleSubmitFeedback(e: React.FormEvent) {
         e.preventDefault();
         console.log(screenshot, comment);
+
+        onFeedbackSent();
     }
 
     return (
@@ -41,7 +44,7 @@ export function FeedbackContentStep({ feedbackType, onRestartFeedback }: Feedbac
             <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
                 <textarea
                     onChange={e => setComment(e.target.value)}
-                    className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-violet-500 focus:ring-violet-300 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
+                    className='min-w-[250px] w-full min-h-[112px] text-sm placeholder-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-violet-500 focus:ring-violet-300 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
                 />
 
 
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
 
 export type FeedbackType = keyof typeof feedbackTypes
 
@@ -31,19 +32,30 @@ export const feedbackTypes = {
 
 export function Widgetform() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState<boolean>(false)
 
-    const handleRestartFeedback = () => setFeedbackType(null);
+    const handleRestartFeedback = () => {
+        setFeedbackSent(false)
+        setFeedbackType(null);
+    }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            {feedbackSent ? (
+                <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback} />
             ) : (
-                <FeedbackContentStep
-                    feedbackType={feedbackType}
-                    onRestartFeedback={handleRestartFeedback}
-                />
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep
+                            feedbackType={feedbackType}
+                            onRestartFeedback={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
             )}
 
             <footer className="text-xs text-neutral-400">
