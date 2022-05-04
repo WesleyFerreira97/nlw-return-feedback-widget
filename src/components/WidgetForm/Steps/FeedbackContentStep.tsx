b5c3ed9365a40edc3d781582e80f64763d1aa566
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-05-03 22:39:29 -0300

    feature send form added

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from 'phosphor-react';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../CloseButton';
 import { ScreenshotButton } from '../ScreenshotButton';
@@ -12,7 +12,12 @@ interface FeedbackContentStepProps {
 export function FeedbackContentStep({ feedbackType, onRestartFeedback }: FeedbackContentStepProps) {
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     const [screenshot, setScreenshot] = useState<string | null>(null);
-    console.log(screenshot);
+    const [comment, setComment] = useState('');
+
+    function handleSubmitFeedback(e: React.FormEvent) {
+        e.preventDefault();
+        console.log(screenshot, comment);
+    }
 
     return (
         <>
@@ -33,25 +38,28 @@ export function FeedbackContentStep({ feedbackType, onRestartFeedback }: Feedbac
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
+            <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
                 <textarea
+                    onChange={e => setComment(e.target.value)}
                     className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-violet-500 focus:ring-violet-300 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
                 />
+
+
+                <footer className='flex gap-2 mt-2'>
+                    <ScreenshotButton
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
+
+                    <button
+                        disabled={comment.length === 0}
+                        type="submit"
+                        className='p-2 bg-violet-600 rounded-md border-transparent flex-1 flex justify-center items-center text-white font-bold text-sm hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-colors duration-300 ease-in-out disabled:opacity-50 disabled:hover:bg-violet-600'
+                    >
+                        Enviar Formulário
+                    </button>
+                </footer>
             </form>
-
-            <footer className='flex gap-2 mt-2'>
-                <ScreenshotButton
-                    screenshot={screenshot}
-                    onScreenshotTook={setScreenshot}
-                />
-
-                <button
-                    type='submit'
-                    className='p-2 bg-violet-600 rounded-md border-transparent flex-1 flex justify-center items-center text-white font-bold text-sm hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-colors duration-300 ease-in-out'
-                >
-                    Enviar Formulário
-                </button>
-            </footer>
 
         </>
     )
