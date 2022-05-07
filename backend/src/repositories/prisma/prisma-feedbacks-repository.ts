4cffd3cddd4d95ee538041b300bef9ecef6ac4bc
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-05-06 22:37:54 -0300

    refactoring app

---
+++ backend/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -0,0 +1,7 @@
+import { FeedbackCreateData, FeedbackRepository } from "../feedbacks-repository";
+
+export class PrismaFeedbacksRepository implements FeedbackRepository {
+    create(data: FeedbackCreateData) {
+
+    }
+}
