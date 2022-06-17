7b3e7ff55b6f70acba9988dac28e72a4a2f8c435
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-06-17 19:17:42 -0300

    init tests Jest

--- backend/src/services/submit-feedback.spec.ts
+++ backend/src/services/submit-feedback.spec.ts
@@ -1,3 +1,18 @@
-test('2 + 2', () => {
-    expect(2 + 2).toBe(4)
+import { SubmitFeedbackService } from './submit-feedback'
+
+describe('Submit Feedback', () => {
+    it('should be able to submit a feedback', async () => {
+        const submitFeedback = new SubmitFeedbackService(
+            { create: async () => { } },
+            { sendMail: async () => { } }
+        );
+
+        await expect(submitFeedback.handle({
+            type: 'Bug',
+            comment: 'Example',
+            screenshot: 'test.jpg'
+        })).resolves.not.toThrow();
+    })
 })
+
+
